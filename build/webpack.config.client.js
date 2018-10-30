a4f7bd7d0fe3f93d0604ab18db2591666328da90
zqc <172232502@qq.com>
2018-10-31 00:35:39 +0800

    begin vue-router

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -21,7 +21,9 @@ const defaultPlugins = [
       NODE_ENV: isDev ? '"development"' : '"production"'
     }
   }),
-  new HTMLPlugin(),
+  new HTMLPlugin({
+    template: path.join(__dirname, 'template.html')
+  }),
   new VueLoaderPlugin()
 ]
 let config
